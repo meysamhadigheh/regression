e2f2caec97c218d85d6f96463262313451e0adb1
meysamhadigheh <hadigheh.meysam@gmail.com>
2021-03-22 17:43:18 +0430

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+import quandl as Quandl
+import pandas as pd
+
+df = Quandl.get('WIKI/GOOGL')
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. Open'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+
+print(df.head())
