afd411c369faa7fc2430620ac581c24d9d9ccc37
meysamhadigheh <hadigheh.meysam@gmail.com>
2021-03-24 19:32:03 +0430

    Pickling and Scaling

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
 
+import pickle
+
 style.use('ggplot')
 
 df = Quandl.get('WIKI/GOOGL')
@@ -32,20 +34,25 @@ X_lately = X[-forecast_out:]
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
-y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
 # -1 as many as it can by processor
-clf = LinearRegression(n_jobs=-1)
+# clf = LinearRegression(n_jobs=-1)
 # clf = svm.SVR()
 
 # polynomial
 # clf = svm.SVR(kernel='poly')
 
 # train
-clf.fit(X_train, y_train)
+# clf.fit(X_train, y_train)
 
+
+# this codes is to save last clf so next time it take less time for process (less time for training classifier)
+# with open('linearregression.pickle', 'wb') as f:
+#     pickle.dump(clf, f)
+pickle_in = open('linearregression.pickle','rb')
+clf=pickle.load(pickle_in)
 # test
 accuracy = clf.score(X_test, y_test)
 
@@ -65,7 +72,6 @@ for i in forecast_set:
     next_date = datetime.datetime.fromtimestamp(next_unix)
     next_unix += one_day
     df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
-
 
 df['Adj. Close'].plot()
 df['Forecast'].plot()
