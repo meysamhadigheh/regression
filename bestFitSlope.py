af18e62a343e389670748f9f021fb5553967b821
meysamhadigheh <hadigheh.meysam@gmail.com>
2021-03-24 20:51:01 +0430

    create our own regression

--- bestFitSlope.py
+++ bestFitSlope.py
@@ -1,10 +1,32 @@
 from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import style
 
-xs = [1, 2, 3, 4, 5, 6]
-ys = [5, 4, 6, 5, 6, 7]
+style.use('fivethirtyeight')
+xs = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
+ys = np.array([5, 4, 6, 5, 6, 7], dtype=np.float64)
 
-plt.plot(xs,ys)
+
+# plt.plot(xs,ys)
+# plt.scatter(xs,ys)
+# plt.show()
+
+def best_fit_slope_and_intercept(xs, ys):
+    numerator = (np.mean(xs) * np.mean(ys)) - np.mean(xs * ys)
+    denominator = np.mean(xs) ** 2 - np.mean(xs ** 2)
+
+    m = numerator / denominator
+
+    b = mean(ys) - m * mean(xs)
+
+    return m, b
+
+
+m, b = best_fit_slope_and_intercept(xs, ys)
+
+regression_line = [(m * x) + b for x in xs]
+
+plt.scatter(xs, ys)
+plt.plot(xs, regression_line)
 plt.show()
-
