cc64f0e49389f76b18a92eac1b8c4d802846ad5c
meysamhadigheh <hadigheh.meysam@gmail.com>
2021-03-25 16:43:19 +0430

    create random dataset via random library

--- bestFitSlope.py
+++ bestFitSlope.py
@@ -3,9 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
 
+import random
+
 style.use('fivethirtyeight')
-xs = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
-ys = np.array([5, 4, 6, 5, 6, 7], dtype=np.float64)
+# xs = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
+# ys = np.array([5, 4, 6, 5, 6, 7], dtype=np.float64)
+
+"""
+    hm => how much
+    correlation it can be positive or negative
+"""
+
+
+def create_dataset(hm, variance, step=2, correlation=False):
+    val = 1
+    ys = []
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val += step
+        elif correlation and correlation == 'neg':
+            val -= step
+
+    xs = [i for i in range(len(ys))]
+
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
 
 # plt.plot(xs,ys)
@@ -33,6 +56,8 @@ def coefficient_of_determination(ys_orig, ys_line):
     squared_error_y_mean = squared_error(ys_orig, y_mean_line)
     return 1 - (squared_error_regr / squared_error_y_mean)
 
+
+xs,ys=create_dataset(40,40,2,correlation='pos')
 
 m, b = best_fit_slope_and_intercept(xs, ys)
 
