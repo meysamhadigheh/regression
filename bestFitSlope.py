de1d3d968d5b93f898629575bac7fc3f6d6469f5
meysamhadigheh <hadigheh.meysam@gmail.com>
2021-03-25 15:56:11 +0430

    add R Square

--- bestFitSlope.py
+++ bestFitSlope.py
@@ -23,6 +23,17 @@ def best_fit_slope_and_intercept(xs, ys):
     return m, b
 
 
+def squared_error(ys_orig, ys_line):
+    return sum((ys_line - ys_orig) ** 2)
+
+
+def coefficient_of_determination(ys_orig, ys_line):
+    y_mean_line = [mean(ys_orig) for _ in ys_orig]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+    return 1 - (squared_error_regr / squared_error_y_mean)
+
+
 m, b = best_fit_slope_and_intercept(xs, ys)
 
 regression_line = [(m * x) + b for x in xs]
@@ -30,9 +41,12 @@ regression_line = [(m * x) + b for x in xs]
 predict_x = 8
 predict_y = (m * predict_x) + b
 
+r_squared = coefficient_of_determination(ys, regression_line)
+
+print(r_squared)
 
 plt.scatter(xs, ys)
 
-plt.scatter(predict_x,predict_y)
+plt.scatter(predict_x, predict_y)
 plt.plot(xs, regression_line)
 plt.show()
