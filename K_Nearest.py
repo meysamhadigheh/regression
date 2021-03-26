7f61f221abc30a7d470f4c641e5221094c384b08
meysamhadigheh <hadigheh.meysam@gmail.com>
2021-03-26 17:54:59 +0430

    SVM with SKLearn

--- K_Nearest.py
+++ K_Nearest.py
@@ -2,24 +2,30 @@ import numpy as np
 from sklearn import preprocessing, model_selection as cross_validation, neighbors
 import pandas as pd
 
-df = pd.read_csv('breast-cancer-wisconsin.data')
-df.replace('?', -99999, inplace=True)
-df.drop(['id'], 1, inplace=True)
 
-X = np.array(df.drop(['class'], 1))
-y = np.array(df['class'])
+accuracies=[]
+for i in range(25):
+    df = pd.read_csv('breast-cancer-wisconsin.data')
+    df.replace('?', -99999, inplace=True)
+    df.drop(['id'], 1, inplace=True)
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+    X = np.array(df.drop(['class'], 1))
+    y = np.array(df['class'])
 
-clf = neighbors.KNeighborsClassifier()
-clf.fit(X_train, y_train)
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-accuracy = clf.score(X_test, y_test)
+    clf = neighbors.KNeighborsClassifier()
+    clf.fit(X_train, y_train)
 
-print(accuracy)
+    accuracy = clf.score(X_test, y_test)
 
-example_measures = np.array([[4, 2, 1, 1, 1, 2, 3, 2, 1],[4, 2, 1, 2, 2, 2, 3, 2, 1]])
-example_measures = example_measures.reshape(len(example_measures), -1)
+    # # print(accuracy)
+    #
+    # example_measures = np.array([[4, 2, 1, 1, 1, 2, 3, 2, 1],[4, 2, 1, 2, 2, 2, 3, 2, 1]])
+    # example_measures = example_measures.reshape(len(example_measures), -1)
+    #
+    # prediction = clf.predict(example_measures)
+    # # print(prediction)
 
-prediction = clf.predict(example_measures)
-print(prediction)
+    accuracies.append(accuracy)
+print(sum(accuracies)/len(accuracies))
--- K_Nearest2.py
+++ K_Nearest2.py
@@ -23,34 +23,37 @@ def k_nearest_neighbors(data, predict, k=3):
 
     return vote_result,confidence
 
+accuracies=[]
+for i in range(25):
+    df = pd.read_csv('breast-cancer-wisconsin.data')
+    df.replace('?', -99999, inplace=True)
+    df.drop(['id'], 1, inplace=True)
+    full_data = df.astype(float).values.tolist()
 
-df = pd.read_csv('breast-cancer-wisconsin.data')
-df.replace('?', -99999, inplace=True)
-df.drop(['id'], 1, inplace=True)
-full_data = df.astype(float).values.tolist()
+    random.shuffle(full_data)
 
-random.shuffle(full_data)
+    test_size = 0.2
+    train_set = {2: [], 4: []}
+    test_set = {2: [], 4: []}
+    train_data = full_data[:-int(test_size * len(full_data))]
+    test_data = full_data[:-int(test_size * len(full_data)):]
 
-test_size = 0.2
-train_set = {2: [], 4: []}
-test_set = {2: [], 4: []}
-train_data = full_data[:-int(test_size * len(full_data))]
-test_data = full_data[:-int(test_size * len(full_data)):]
+    for i in train_data:
+        train_set[i[-1]].append(i[:-1])
 
-for i in train_data:
-    train_set[i[-1]].append(i[:-1])
+    for i in test_data:
+        test_set[i[-1]].append(i[:-1])
 
-for i in test_data:
-    test_set[i[-1]].append(i[:-1])
-
-correct = 0
-total = 0
-for group in test_set:
-    for data in test_set[group]:
-        vote,confidence = k_nearest_neighbors(train_set, data, k=5)
-        if group == vote:
-            correct += 1
-        else:
-            print(confidence)
-        total += 1
-print('Accuracy: ',correct/total)
+    correct = 0
+    total = 0
+    for group in test_set:
+        for data in test_set[group]:
+            vote,confidence = k_nearest_neighbors(train_set, data, k=5)
+            if group == vote:
+                correct += 1
+            # else:
+            #     print(confidence)
+            total += 1
+    print('Accuracy: ',correct/total)
+    accuracies.append(correct/total)
+print(sum(accuracies)/len(accuracies))
